a37d9fa7c4200a1d0550b8171748457e7418c22b
alamba1 <57027220+alamba1@users.noreply.github.com>
2021-05-24 18:52:26 -0700

    Update signal_processing_new.py

--- signal_processing_new.py
+++ signal_processing_new.py
@@ -12,19 +12,27 @@ tlow, thigh = 4, 8
 
 def compute_periodogram(eegdata):
   #Find the power spectral density of the data
+  global p_freqs
+  global p_psd
+  global p_freq_res
+  global p_theta_power
+  global p_total_power
+  global p_theta_rel_power
+
   p_freqs, p_psd = signal.periodogram(eegdata, srate)
-  #Frequency resolution (bin size)
-  p_freq_res = p_freqs[1] - p_freqs[0]
-  #Find intersecting values in frequency vector
-  p_idx_theta = np.logical_and(p_freqs >= tlow, p_freqs <= thigh)
+  if p_freqs.size > 0:
+    #Frequency resolution (bin size)
+    p_freq_res = p_freqs[1] - p_freqs[0] 
+    #Find intersecting values in frequency vector
+    p_idx_theta = np.logical_and(p_freqs >= tlow, p_freqs <= thigh)
   
-  # Compute the absolute power by approximating the area under the curve
-  p_theta_power = simps(p_psd[p_idx_theta], dx=p_freq_res)
+    # Compute the absolute power by approximating the area under the curve
+    p_theta_power = simps(p_psd[p_idx_theta], dx=p_freq_res)
 
     # Relative power (expressed as a percentage of total power)
-  p_total_power = simps(p_psd, dx=p_freq_res)
+    p_total_power = simps(p_psd, dx=p_freq_res)
 
-  p_theta_rel_power = p_theta_power / p_total_power
+    p_theta_rel_power = p_theta_power / p_total_power
   
   return(pd.DataFrame([p_theta_rel_power]))
 
@@ -56,75 +64,74 @@ def compute_Welch(data):
   return(pd.DataFrame([w_theta_rel_power]))
 
 
-#obtain data
-data = pd.read_csv('/Users/willstonehouse/Desktop/CruXProject/baseline.txt', sep = ',',header = 4, usecols = [" EXG Channel 0", " EXG Channel 1"])
-data = data.dropna()
 srate = 200
+filename = '/Users/Ashley/Desktop/CruX/OpenBCI-RAW-2021-05-10_19-10-40.txt'
+
+data = np.loadtxt(filename, delimiter=',', skiprows = 6, usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8))
+
+index = data[:, 0]
+exg_0 = data[:, 1]
+exg_1 = data[:, 2]
+exg_2 = data[:, 3]
+exg_3 = data[:, 4]
+accel_0 = data[:, 5]
+accel_1 = data[:, 6]
+accel_2 = data[:, 7]
+
+print(data[0])
+
+#plot graph
+colOfInterest = exg_0
+
+fs = 200
+colOfInterest = colOfInterest - np.mean(colOfInterest)
+col_fft = np.abs(np.fft.fft(colOfInterest))
+col_freq = np.fft.fftfreq(len(col_fft), 1/fs)
+plt.plot(col_freq, col_fft)
+plt.xlim([5, 10])
+plt.ylim(0, 16000)
 
 #Calculating Welch's method over first 1 minute to get baseline theta values
-baseline_eeg_right = data.iloc[0:srate*60," EXG Channel 0"]
-baseline_eeg_left = data.iloc[0:srate*60," EXG Channel 1"]
+baseline_eeg_midline = data[0:srate*30, 0]
 
-w_rel_power_right = pd.DataFrame.transpose(compute_Welch(baseline_eeg_right))
-w_rel_power_left = pd.DataFrame.transpose(compute_Welch(baseline_eeg_left))
+w_rel_power_midline = pd.DataFrame.transpose(compute_Welch(baseline_eeg_midline))
   
-w_rel_power_right.columns = ["Theta_Rel"]
-w_rel_power_left.columns = ["Theta_Rel"]
+w_rel_power_midline.columns = ["Theta_Rel"]
 
-powers_eeg_right= pd.DataFrame()
-powers_eeg_left= pd.DataFrame() 
+powers_eeg_midline= pd.DataFrame()
 
-'''
-#right = data[' EXG Channel 0']
-#left = data[' EXG Channel 1']
-[left, right] = compute_Welch(left, right)
-'''
+
 #iterate such that 50% overlap, so start at 200 data points after the previous starting point (i.e. 1 second after)
 #look at 2nd minute's worth of data, starting at 60 seconds and 50% overlap till last interval
 #we could find length of time in seconds: len(data) / srate -> number of iterations for the for loop  (seconds - 1)
+lengthData = len(data)//srate
+myList = range(40, lengthData)
 
-for starting_sec in range(60,[len(data)-1]) :
+for starting_sec in myList:
   start = starting_sec * srate #start of each 2 seconds worth of data
   end = (starting_sec * srate) + (srate * 2) #end, srate*2 = 2 seconds worth of data
   
   #in case we don't get enough data points for the last iteration
   if(end > len(data)):
-    binaural_eeg_right = data.iloc[start:len(data), " EXG Channel 0"]
-    binaural_eeg_left = data.iloc[start:len(data), " EXG Channel 1"]
+    binaural_eeg_midline = data[start:len(data), 0]
+ 
   else:
-    binaural_eeg_right = data.iloc[start:end, " EXG Channel 0"]
-    binaural_eeg_left = data.iloc[start:end, " EXG Channel 1"]
+    binaural_eeg_midline = data[start:end, 0]
   
   #get the powers from periodogram
-  binaural_right_powers = pd.DataFrame.transpose(compute_periodogram(binaural_eeg_right))
-  binaural_left_powers = pd.DataFrame.transpose(compute_periodogram(binaural_eeg_left))
+  binaural_midline_powers = pd.DataFrame.transpose(compute_periodogram(binaural_eeg_midline))
   
   #add the values to dataframe for future calculations/reference
-  powers_eeg_right = pd.concat([powers_eeg_right,binaural_right_powers], ignore_index = True)
-  powers_eeg_left = pd.concat([powers_eeg_left,binaural_left_powers], ignore_index = True)
+  powers_eeg_midline = pd.concat([powers_eeg_midline,binaural_midline_powers], ignore_index = True)
 
 #relative theta powers separated by electrode
-powers_eeg_right.columns = ["Theta_Rel"]
-powers_eeg_left.columns = ["Theta_Rel"]
+powers_eeg_midline.columns = ["Theta_Rel"]
 
 #combined relative theta powers
-theta = (powers_eeg_right.iloc[:, 0].values + powers_eeg_left.iloc[:, 0].values)/2
+theta_midline = powers_eeg_midline.iloc[:, 1].values
 
-right_theta = w_rel_power_right.iat[0,0]
-left_theta = w_rel_power_left.iat[0,0]
+midline_theta_total = w_rel_power_midline.iat[0,0]
 
-#average baseline theta powers
-theta_total = (right_theta + left_theta)/2
+baselined_theta = pd.DataFrame(((theta_midline - midline_theta_total)/midline_theta_total*100))
 
-baselined_theta = pd.DataFrame(((theta - theta_total)/theta_total)*100)
-
-#To simulate streaming, we update our database periodically with the values
-#given in the sequences
-request_url = "http://localhost:8010/proxy/test/"
- 
-theta_percent_change = baselined_theta.values[:, 0]
-
-for i in range(len(theta_percent_change)): 
-    requests.put(request_url + "theta", json={'value': round(theta_percent_change[i], 3)})
-    time.sleep(2)
-
+print(baselined_theta)
